ea799e36bd72abbdfd5fadd54d5b0770bd23e7fb
Astraspire <124364679+Astraspire@users.noreply.github.com>
2025-06-22 10:13:31 -0400

    cleaned up fades and loop changing

--- MBC01System/SongManager.ts
+++ MBC01System/SongManager.ts
@@ -28,51 +28,53 @@ class SongManager extends hz.Component<typeof SongManager> {
         chan5Loop3: { type: hz.PropTypes.Entity },
         chan5Loop4: { type: hz.PropTypes.Entity },
         chan5Loop5: { type: hz.PropTypes.Entity },
+        customFadeTime: { type: hz.PropTypes.Number },
+        customBPM: { type: hz.PropTypes.Number },
+        beatsPerLoop: { type: hz.PropTypes.Number },
+
     };
 
-    // number of channels on MBC-25 controller
-    private static readonly NUM_LOOPS: number = 5;
-    // cross-fade time in seconds
-    private static readonly FADE_TIME = 2.0;
-
-    // Defines song BPM for script
-    private static readonly SONG_BPM = 120
-    // Defines beatsPerLoop for script
-    private static readonly BEATS_PER_LOOP = 32;
-
-    // Map of channels
-    private channelLoops: hz.AudioGizmo[][] = [];
-
-    // map of channel IDs -> currently active AudioGizmo on that channel
+    private fadeTime!: number;
+    private songBPM!: number;
+    private beatsPerLoop!: number;
+    private channelLoops!: hz.AudioGizmo[][];
     private activeLoops: Record<number, hz.AudioGizmo> = {};
+    private loopDurationSec!: number;
 
-
-    // decides loop duration based on properties set
-    private loopDurationSec = (60 / SongManager.SONG_BPM) * SongManager.BEATS_PER_LOOP; 
-
-    private stopChannel = (channelId: number) => {
-        console.log(`Channel ${channelId} triggered to stop.`)
-
-        // stops and removes channel from activeLoops list
-        const oldLoop = this.activeLoops[channelId];
-        if (oldLoop) {
-            oldLoop.stop();
-            delete this.activeLoops[channelId];
-        }
-    }
 
     override preStart() {
+        // maps all audio gizmos to the channel grid
+        this.channelLoops = [
+            [this.props.chan1Loop1!.as(hz.AudioGizmo), this.props.chan1Loop2!.as(hz.AudioGizmo), this.props.chan1Loop3!.as(hz.AudioGizmo), this.props.chan1Loop4!.as(hz.AudioGizmo), this.props.chan1Loop5!.as(hz.AudioGizmo)],
+            [this.props.chan2Loop1!.as(hz.AudioGizmo), this.props.chan2Loop2!.as(hz.AudioGizmo), this.props.chan2Loop3!.as(hz.AudioGizmo), this.props.chan2Loop4!.as(hz.AudioGizmo), this.props.chan2Loop5!.as(hz.AudioGizmo)],
+            [this.props.chan3Loop1!.as(hz.AudioGizmo), this.props.chan3Loop2!.as(hz.AudioGizmo), this.props.chan3Loop3!.as(hz.AudioGizmo), this.props.chan3Loop4!.as(hz.AudioGizmo), this.props.chan3Loop5!.as(hz.AudioGizmo)],
+            [this.props.chan4Loop1!.as(hz.AudioGizmo), this.props.chan4Loop2!.as(hz.AudioGizmo), this.props.chan4Loop3!.as(hz.AudioGizmo), this.props.chan4Loop4!.as(hz.AudioGizmo), this.props.chan4Loop5!.as(hz.AudioGizmo)],
+            [this.props.chan5Loop1!.as(hz.AudioGizmo), this.props.chan5Loop2!.as(hz.AudioGizmo), this.props.chan5Loop3!.as(hz.AudioGizmo), this.props.chan5Loop4!.as(hz.AudioGizmo), this.props.chan5Loop5!.as(hz.AudioGizmo)],
+        ];
+
+        // cross-fade time in seconds
+        this.fadeTime = this.props.customFadeTime!;
+
+        // Defines song BPM for script
+        this.songBPM = this.props.customBPM!;
+
+        // Defines beatsPerLoop for script
+        this.beatsPerLoop = this.props.beatsPerLoop!;
+
+        // decides loop duration based on properties set
+        this.loopDurationSec = (60 / this.songBPM) * this.beatsPerLoop;
 
         // Listen for loopTriggerEvent
         this.connectLocalBroadcastEvent(
             loopTriggerEvent, (loopData) => {
                 console.log(`Channel: ${loopData.channelId}, Loop: ${loopData.loopSectionId} triggered to start.`);
-
+               
                 // Stop any old loop on that channel
                 const oldLoop = this.activeLoops[loopData.channelId];
                 if (oldLoop) {
-                    oldLoop.stop({ fade: SongManager.FADE_TIME});
+                    this.removeLoop(loopData.channelId);
                 }
+
                 // assigns for playback
                 const newAudio = this.channelLoops[loopData.channelId - 1][loopData.loopSectionId - 1];
 
@@ -81,26 +83,19 @@ class SongManager extends hz.Component<typeof SongManager> {
             }
         );
 
-        // Listen for stopRowEvent
+        // Listen for stopRowEvent / stops channel
         this.connectLocalBroadcastEvent(
             stopRowEvent, (channelData) => {
                 this.stopChannel(channelData.channelId);
+
             }
         );
 
     }
 
     override start() {
-        const interval = this.loopDurationSec * 1000; // converts to ms
 
-        // maps all audio gizmos to the channel grid
-        this.channelLoops = [
-            [this.props.chan1Loop1!.as(hz.AudioGizmo), this.props.chan1Loop2!.as(hz.AudioGizmo), this.props.chan1Loop3!.as(hz.AudioGizmo), this.props.chan1Loop4!.as(hz.AudioGizmo), this.props.chan1Loop5!.as(hz.AudioGizmo)],
-            [this.props.chan2Loop1!.as(hz.AudioGizmo), this.props.chan2Loop2!.as(hz.AudioGizmo), this.props.chan2Loop3!.as(hz.AudioGizmo), this.props.chan2Loop4!.as(hz.AudioGizmo), this.props.chan2Loop5!.as(hz.AudioGizmo)],
-            [this.props.chan3Loop1!.as(hz.AudioGizmo), this.props.chan3Loop2!.as(hz.AudioGizmo), this.props.chan3Loop3!.as(hz.AudioGizmo), this.props.chan3Loop4!.as(hz.AudioGizmo), this.props.chan3Loop5!.as(hz.AudioGizmo)],
-            [this.props.chan4Loop1!.as(hz.AudioGizmo), this.props.chan3Loop2!.as(hz.AudioGizmo), this.props.chan4Loop3!.as(hz.AudioGizmo), this.props.chan4Loop4!.as(hz.AudioGizmo), this.props.chan4Loop5!.as(hz.AudioGizmo)],
-            [this.props.chan5Loop1!.as(hz.AudioGizmo), this.props.chan3Loop2!.as(hz.AudioGizmo), this.props.chan5Loop3!.as(hz.AudioGizmo), this.props.chan5Loop4!.as(hz.AudioGizmo), this.props.chan5Loop5!.as(hz.AudioGizmo)],
-        ];
+        const interval = this.loopDurationSec * 1000; // converts to ms
 
         // Every interval ms, replay every active loop so they stay in lock-step
         this.async.setInterval(() => {
@@ -110,5 +105,29 @@ class SongManager extends hz.Component<typeof SongManager> {
         }, interval);
     }
 
+    private removeLoop = (channelId: number) => {
+        console.log(`Channel ${channelId} triggered to stop.`)
+
+        // stops and removes channel from activeLoops list
+        const oldLoop = this.activeLoops[channelId];
+        if (oldLoop) {
+ 
+            delete this.activeLoops[channelId];
+        }
+    }
+
+    private stopChannel = (channelId: number) => {
+        console.log(`Channel ${channelId} triggered to stop.`)
+
+        // stops and removes channel from activeLoops list
+        const oldLoop = this.activeLoops[channelId];
+        if (oldLoop) {
+            // removes loop from active loops
+            delete this.activeLoops[channelId];
+
+            // stops and fades out playing loop on channel
+            oldLoop.stop({ fade: this.fadeTime });
+        }
+    }
 }
 hz.Component.register(SongManager);
